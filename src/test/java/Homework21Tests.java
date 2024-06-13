530ae394e9c0892fc0385c3435cdb9a301ece228
ochaklosh <oksana.chaklosh@testpro.io>
2024-06-13 01:20:31 -0400

    Created Homework21Tests
    This is an updated version of the rename Playlist method created previously
    I added method to create a new Play list
    Add a song to it
    Rename the playlist

--- src/test/java/Homework21Tests.java
+++ src/test/java/Homework21Tests.java
@@ -1,26 +1,36 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework21Tests extends BaseTest {
+    String editedPlaylistName = "homework21";
+    String expectedPlaylistUpdatedMessage = "Updated playlist \"homework21.\"";
+    String newPlaylistName = "Playlist21";
 
     @Test
     public void improvedRenamePlaylist() {
 
-        String expectedSongDeletedMessage = "Updated playlist \"homework21.\"";
-        String newPlaylistName = "homework21";
-
-        //Login
+    //Login
         enterEmail(email);
         enterPassword(password);
         submit();
-        //Create New Playlist
+    //Create New Playlist
         createNewPlaylist(newPlaylistName);
-        //Add songs to the Playlist
+    //Wait for successful message to hide
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.hide")));
+    //Add songs to the Playlist
         addSongToPlaylist();
+    //Update Playlist Name
+        changePlaylistName();
+     //Wait for successful message to hide
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.hide")));
+        Assert.assertEquals(getRenamePlaylistSuccessMsg(), expectedPlaylistUpdatedMessage);
     }
+
 
     private void createNewPlaylist(String newPlaylistName) {
         wait.until(ExpectedConditions.elementToBeClickable
@@ -32,37 +42,29 @@ public class Homework21Tests extends BaseTest {
         inputNewPlaylist.sendKeys(newPlaylistName);
         inputNewPlaylist.submit();
     }
-
+///////  Add Song To Play list methods
     public void addSongToPlaylist(){
         //search for song
         searchSong("Music");
         //click view all button
         clickViewAllButton();
-        System.out.println("The size is   " + wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.cssSelector("section#playlistWrapper td.title"))).getSize());
-        //
-        //select first song from results
-        //clickFirstSongInList();
-        //click add to button
-        //clickAddToButton();
-        //choose a playlist from menu to add song
-        //choosePlaylist();
-
+        //for (int i=0; i<3; i++ ) {
+            clickFirstSongInList();
+            clickAddToButton();
+            choosePlaylistToAddTo();
+       // }
     }
 
-
-
-    private void choosePlaylist() {
-        //WebElement listItem = driver.findElement
-        // (By.cssSelector("#songsWrapper .existing-playlists ul :nth-child(5)"));
-        WebElement listItem = driver.findElement
-                (By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'playlist2024')]"));
-        listItem.click();
+    public void searchSong(String song) {
+        WebElement searchField = wait.until(ExpectedConditions.elementToBeClickable
+                (By.cssSelector("input[type='search']")));
+        searchField.clear();
+        searchField.sendKeys(song);
     }
 
-    private void clickAddToButton() {
-        WebElement addToListButton = driver.findElement(By.cssSelector("#songResultsWrapper button[data-test='add-to-btn']"));
-        addToListButton.click();
+    private void clickViewAllButton() {
+        wait.until(ExpectedConditions.elementToBeClickable
+                (By.cssSelector("[data-test='view-all-songs-btn']"))).click();
     }
 
     private void clickFirstSongInList() {
@@ -70,17 +72,47 @@ public class Homework21Tests extends BaseTest {
         firstSong.click();
     }
 
-    private void clickViewAllButton() {
-       wait.until(ExpectedConditions.elementToBeClickable
-                (By.cssSelector("[data-test='view-all-songs-btn']"))).click();
+    private void clickAddToButton() {
+        WebElement addToListButton = driver.findElement(By.cssSelector("#songResultsWrapper button[data-test='add-to-btn']"));
+        addToListButton.click();
     }
 
+    private void choosePlaylistToAddTo() {
+        wait.until(ExpectedConditions.elementToBeClickable
+           (By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Playlist21')]"))).click();
+    }
 
-    public void searchSong(String song) {
-        WebElement searchField = wait.until(ExpectedConditions.elementToBeClickable
-                (By.cssSelector("input[type='search']")));
-        searchField.clear();
-        searchField.sendKeys(song);
+/////////  Change the Name of the Play List
+
+    private void changePlaylistName() {
+    choosePlaylist();
+    doubleClickPlaylist();
+    enterNewName();
+    }
+
+    private void choosePlaylist() {
+        wait.until(ExpectedConditions.elementToBeClickable
+                (By.xpath("//a[contains(text(),'Playlist21')]"))).click();
+    }
+
+    private void doubleClickPlaylist() {
+        WebElement playlist = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[contains(text(),'Playlist21')]")));
+        actions.doubleClick(playlist).perform();
+    }
+
+    private void enterNewName() {
+        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("[name='name']")));
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL, "A"), Keys.BACK_SPACE);
+        playlistInputField.sendKeys(editedPlaylistName);
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
+
+    public String getRenamePlaylistSuccessMsg(){
+        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div.success.show")));
+        return notification.getText();
     }
 
 }
