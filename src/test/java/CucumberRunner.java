0a8159d25dfeba35bd289f823b1173d4beef90f7
ochaklosh <oksana.chaklosh@testpro.io>
2024-06-27 00:58:07 -0400

    Created CucumberRunner
    Created CucumberRunner a third needed file to run Cucumber, to be able to use Login.feature

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -1,9 +1,11 @@
-/*
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testing.TestNGCucumberRunner;
+import io.cucumber.testng.TestNGCucumberRunner;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(features = {"src/test/resources/features/Login.feature"})
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
     private TestNGCucumberRunner testNGCucumberRunner;
@@ -13,5 +15,15 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
 
+    @DataProvider
+    public Object[][] features(){
+        return testNGCucumberRunner.provideScenarios();
+    }
 
-}*/
+    @AfterClass
+    public void tearDownClass(){
+        testNGCucumberRunner.finish();
+    }
+
+
+}
